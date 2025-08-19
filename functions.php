d95477f5648ce30f2b667e93d6295c32787ad3ed
Sam <hello@darewp.com>
2025-08-19 23:02:34 +0800

    Init func template file

--- functions.php
+++ functions.php
@@ -1,3 +1,7 @@
 <?php
+/* 
+Template file: Functions.php
+This file contains the core functions and classes for the theme.
+*/
 require get_template_directory() . '/includes/traits/Singleton.php';
 require get_template_directory() . '/includes/classes/AssetLoader.php';
