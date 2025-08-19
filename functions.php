865927bc2118307763b03cc727b44a5cd97930b1
Sam <hello@darewp.com>
2025-08-20 00:17:59 +0800

    Test deployment

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 /* 
 Template file: Functions.php
 This file contains the core functions and classes for the theme.
+Testing deployment and functionality.
 */
 require get_template_directory() . '/includes/traits/Singleton.php';
 require get_template_directory() . '/includes/classes/AssetLoader.php';
