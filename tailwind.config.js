fe63db417d4a619e9f88f368b78b56dc8165c1c2
Sam <hello@darewp.com>
2025-08-23 19:49:41 +0800

    Enqueue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,8 @@ module.exports = {
     "./inc/**/*.php",
     "./template-parts/**/*.php",
   ],
-  safelist: [
-    'ring-orange',
-  ],
   theme: {
-    extend: {
-      colors: {
-        'orange': '#F85E00',
-        'test-pink': '#ff00ff',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
