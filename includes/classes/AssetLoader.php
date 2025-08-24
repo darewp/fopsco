aec4c19f14f619fb3e97d2c0b1266d7111cac240
Sam <hello@darewp.com>
2025-08-24 19:28:49 +0800

    Debug response

--- includes/classes/AssetLoader.php
+++ includes/classes/AssetLoader.php
@@ -13,11 +13,11 @@ class AssetLoader {
     
         wp_enqueue_style( 'fopsco-tailwind', get_template_directory_uri() . '/assets/css/output.css', [], filemtime(get_template_directory() . '/assets/css/output.css') );
 
-        wp_enqueue_script( 'validator', '//cdn.jsdelivr.net/npm/validator@13.9.0/validator.min.js', [], '13.9.0', true );
-        wp_enqueue_script( 'fopsco-join', get_template_directory_uri() . '/assets/js/join.js', ['validator'], filemtime(get_template_directory() . '/assets/js/join.js'), true );
-
         wp_enqueue_script( 'fopsco', get_template_directory_uri() . '/assets/js/main.js', [], filemtime(get_template_directory() . '/assets/js/main.js'), true );
 
+        wp_enqueue_script( 'validator', '//cdn.jsdelivr.net/npm/validator@13.9.0/validator.min.js', [], '13.9.0', true );
+        
+        wp_enqueue_script( 'fopsco-join', get_template_directory_uri() . '/assets/js/join.js', ['validator'], filemtime(get_template_directory() . '/assets/js/join.js'), true );
         wp_localize_script('fopsco-join', 'wpApiSettings', [
             'nonce' => wp_create_nonce('wp_rest')
         ]);
