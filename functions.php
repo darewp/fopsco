5a943698e44d3ba935dc9c733768cd9cc630a255
Sam <hello@darewp.com>
2025-08-24 20:59:50 +0800

    Debug response

--- functions.php
+++ functions.php
@@ -10,10 +10,8 @@ require get_template_directory() . '/includes/classes/SVGSupport.php';
 require get_template_directory() . '/includes/classes/JoinMember.php';
 require get_template_directory() . '/includes/classes/DareWPAuto.php';
 
-add_action('after_setup_theme', function() {
-    // Initialize classes
-    \Fopsco\Classes\AssetLoader::get_instance();
-    \Fopsco\Classes\SVGSupport::get_instance();
-    \Fopsco\Classes\JoinMember::get_instance();
-    \Fopsco\Classes\DareWPAuto::get_instance();
-});
+
+\Fopsco\Classes\AssetLoader::get_instance();
+\Fopsco\Classes\SVGSupport::get_instance();
+\Fopsco\Classes\JoinMember::get_instance();
+\Fopsco\Classes\DareWPAuto::get_instance();
