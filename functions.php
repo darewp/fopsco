540b76d9832fa900450202507e422a9521f652d7
Sam <hello@darewp.com>
2025-08-24 19:50:42 +0800

    Debug response

--- functions.php
+++ functions.php
@@ -11,8 +11,16 @@ require get_template_directory() . '/includes/classes/JoinMember.php';
 require get_template_directory() . '/includes/classes/DareWPAuto.php';
 
 add_action('after_setup_theme', function() {
+    // Initialize classes
     \Fopsco\Classes\AssetLoader::get_instance();
     \Fopsco\Classes\SVGSupport::get_instance();
     \Fopsco\Classes\JoinMember::get_instance();
-    \Fopsco\Classes\DareWPAuto::get_instance();
+    $darewp = \Fopsco\Classes\DareWPAuto::get_instance();
+
+    // Debug: check if constants are available
+    if ( defined('DAREWP_N8N_URL') ) {
+        $darewp->log('DAREWP_N8N_URL = ' . DAREWP_N8N_URL);
+    } else {
+        $darewp->log('DAREWP_N8N_URL NOT DEFINED!');
+    }
 });
