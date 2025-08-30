eb25597f54244debf06ac61ccab32189661c0da3
Sam <hello@darewp.com>
2025-08-31 00:32:56 +0800

    PMES video markup

--- pmes.php
+++ pmes.php
@@ -13,8 +13,8 @@ if ( ! is_user_logged_in() ) {
 wp_head();
 ?>
 
-<div class="flex justify-center items-center px-4 py-8">
-  <div class="max-w-4xl mx-auto px-4">
+<div class="max-w-6xl mx-auto px-4">
+    <div class="flex flex-wrap justify-center mt-4">
     <div class="relative pb-[56.25%] h-0 overflow-hidden rounded-2xl shadow-lg">
       <video 
         id="lessonVideo" 
