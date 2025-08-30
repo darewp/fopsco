b0caa1c755eec975f1d7fccf26b9a980e455afc9
Sam <hello@darewp.com>
2025-08-31 00:47:10 +0800

    PMES video markup

--- pmes.php
+++ pmes.php
@@ -18,7 +18,7 @@ wp_head();
     <video 
       id="lessonVideo" 
       controls 
-      class="absolute top-0 left-0 w-full h-full rounded-2xl"
+      class="absolute top-5 left-0 w-full h-full rounded-2xl"
     >
       <source src="https://portal.smrmnt.com/wp-content/uploads/2025/08/Pre-membership-Education-Seminar-Video.mp4" type="video/mp4">
       Your browser does not support the video tag.
