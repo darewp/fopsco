c3b8bccca1cd06062e2bb17a4c273056d82362a0
Sam <hello@darewp.com>
2025-08-31 01:04:39 +0800

    PMES video markup

--- pmes.php
+++ pmes.php
@@ -12,18 +12,19 @@ if ( ! is_user_logged_in() ) {
 }
 wp_head();
 ?>
-
-<div class="max-w-[60rem] mx-auto px-4 min-h-screen flex items-center justify-center">
-  <div class="relative w-full pb-[56.25%] h-0 overflow-hidden rounded-2xl shadow-lg">
-    <video 
-      id="lessonVideo" 
-      controls 
-      class="absolute top-5 left-0 w-full h-full rounded-2xl"
-    >
-      <source src="https://portal.smrmnt.com/wp-content/uploads/2025/08/Pre-membership-Education-Seminar-Video.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-  </div>
+<div class="max-w-7xl mx-auto px-4">
+    <div class="flex flex-wrap justify-center mt-4">
+        <div class="relative w-full pb-[56.25%] h-0 overflow-hidden rounded-2xl shadow-lg">
+            <video 
+            id="lessonVideo" 
+            controls 
+            class="absolute top-5 left-0 w-full h-full rounded-2xl"
+            >
+            <source src="https://portal.smrmnt.com/wp-content/uploads/2025/08/Pre-membership-Education-Seminar-Video.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
 </div>
 
 
