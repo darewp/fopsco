d1323245cc4c330de87451b311a243c78b24e9e8
Sam <hello@darewp.com>
2025-09-05 16:59:17 +0800

    inline fields

--- join.php
+++ join.php
@@ -15,36 +15,66 @@ if( !is_user_logged_in() ){
             <form id="joinForm" class="space-y-4">
                 <?php wp_nonce_field('lodge_join_form', 'lodge_join_nonce'); ?>
 
-                <div>
-                    <input type="text" id="first_name" placeholder="First Name" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" autocomplete="off" required>
+                <!-- First + Last Name -->
+                <div class="flex space-x-4">
+                    <div class="w-1/2">
+                        <input type="text" id="first_name" placeholder="First Name"
+                            class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                            autocomplete="off" required>
+                    </div>
+                    <div class="w-1/2">
+                        <input type="text" id="last_name" placeholder="Last Name"
+                            class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                            autocomplete="off" required>
+                    </div>
                 </div>
-                <div>
-                    <input type="text" id="last_name" placeholder="Last Name" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" autocomplete="off" required>
+
+                <!-- Email + Phone -->
+                <div class="flex space-x-4">
+                    <div class="w-1/2">
+                        <input type="email" id="email" placeholder="Email Address"
+                            class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                            autocomplete="off" required>
+                    </div>
+                    <div class="w-1/2">
+                        <input type="text" id="phone_number" placeholder="Mobile Number"
+                            class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                            autocomplete="off" required>
+                    </div>
                 </div>
+
+                <!-- Member Type -->
                 <div>
-                    <input type="email" id="email" placeholder="Email Address" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" autocomplete="off" required>
-                </div>                
-                <div>
-                    <input type="text" id="phone_number" placeholder="Mobile Number" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" autocomplete="off" required>
-                </div>
-                <div>
-                    <select id="member_type" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" required>
+                    <select id="member_type"
+                        class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                        required>
                         <option value="" disabled selected>Select Member Type</option>
                         <option value="regular">Online Professional</option>
                         <option value="associate">MSME</option>
                     </select>
-                </div>                
+                </div>
+
+                <!-- Passwords -->
                 <div>
-                    <input type="password" id="password" placeholder="Password" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" autocomplete="off" required>
+                    <input type="password" id="password" placeholder="Password"
+                        class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                        autocomplete="off" required>
                 </div>
                 <div>
-                    <input type="password" id="confirm" placeholder="Confirm Password" class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none" autocomplete="off" required>
+                    <input type="password" id="confirm" placeholder="Confirm Password"
+                        class="w-full px-4 py-2 border rounded-lg focus:ring-1 focus:ring-[#F85E00] focus:border-[#F85E00] focus:outline-none"
+                        autocomplete="off" required>
                 </div>
+
                 <input type="hidden" name="website" id="website" value="">
                 <div id="errors" class="text-red-600 text-sm"></div>
 
-                <button type="submit" class="w-full bg-[#182955] text-white py-3 px-4 rounded-lg hover:bg-[#F85E00] transition">Join Now</button>
+                <button type="submit"
+                    class="w-full bg-[#182955] text-white py-3 px-4 rounded-lg hover:bg-[#F85E00] transition">
+                    Join Now
+                </button>
             </form>
+
         </div>
         
     </div>
