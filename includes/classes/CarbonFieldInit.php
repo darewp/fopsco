90cb8281b569e64b95b07ba81823ad09716c49f5
Sam <hello@darewp.com>
2025-12-03 12:43:58 +0800

    Each field visible - graphql

--- includes/classes/CarbonFieldInit.php
+++ includes/classes/CarbonFieldInit.php
@@ -13,29 +13,39 @@ class CarbonFieldInit {
     }
 
     public function register_fields() {
-        // Committee Container
+    // Committee Container
         Container::make('theme_options', 'Committee')
+            ->set_visible_in_graphql(true)
             ->add_fields([
                 Field::make('complex', 'committee_members', 'Committee Members')
                     ->set_visible_in_graphql(true)
                     ->add_fields([
-                        Field::make('text', 'full_name', 'Full Name'),
-                        Field::make('text', 'role', 'Role'),
+                        Field::make('text', 'full_name', 'Full Name')
+                            ->set_visible_in_graphql(true),
+
+                        Field::make('text', 'role', 'Role')
+                            ->set_visible_in_graphql(true),
+
                         Field::make('image', 'profile_picture', 'Profile Picture')
-                            ->set_help_text('Upload the member\'s profile image.'),
+                            ->set_visible_in_graphql(true),
                     ])
                     ->set_layout('tabbed-horizontal'),
             ]);
-        // Partners Logo
+
+        // Partners Container
         Container::make('theme_options', 'Partners')
+            ->set_visible_in_graphql(true)
             ->add_fields([
                 Field::make('complex', 'partners_logo', 'Partners Logo')
                     ->set_visible_in_graphql(true)
                     ->add_fields([
-                        Field::make('image', 'logo', 'Logo'),
-                        Field::make('text', 'partner_name', 'Partner Name'),
+                        Field::make('image', 'logo', 'Logo')
+                            ->set_visible_in_graphql(true),
+
+                        Field::make('text', 'partner_name', 'Partner Name')
+                            ->set_visible_in_graphql(true),
                     ])
                     ->set_layout('tabbed-horizontal'),
             ]);
-    }
+        }
 }
